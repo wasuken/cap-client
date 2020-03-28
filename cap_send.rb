6718f5987c34cbbc51a77eccf34ef6ff5ae76401
wasu <wevorence@gmail.com>
2020-03-28 19:43:34 +0900

    update url and exception process.

--- cap_send.rb
+++ cap_send.rb
@@ -5,7 +5,8 @@ require "socket"
 include PacketFu
 
 TOKEN = File.open("token").read.gsub(/\n/, '')
-SERVER_PATH = "ws://localhost:3000/cable/netpackets?token=#{TOKEN}"
+URL = File.open("url").read.gsub(/\n/, '')
+SERVER_PATH = "#{URL}?token=#{TOKEN}"
 $opened = false
 $ws = nil
 
@@ -25,43 +26,50 @@ def get_capture_and_send(iface)
     $ws.ping 'living..' do
       # fires when pong is received
     end
-    if TCPPacket.can_parse?(pkt)
-      tcp_packet = TCPPacket.parse(pkt)
-      src_mac = EthHeader.str2mac(tcp_packet.eth_src).to_s
-      dst_mac = EthHeader.str2mac(tcp_packet.eth_dst).to_s
-      src_ip = IPHeader.octet_array(tcp_packet.ip_src).join('.')
-      dst_ip = IPHeader.octet_array(tcp_packet.ip_dst).join('.')
-      src_port = tcp_packet.tcp_src.to_s
-      dst_port = tcp_packet.tcp_dst.to_s
-      # p "tcp => (#{src_mac} - #{dst_mac}) => (#{src_ip} - #{dst_ip}) => (#{src_port} - #{dst_port})"
-      src = {smac: src_mac, sip: src_ip, sport: src_port}
-      dst = {dmac: dst_mac, dip: dst_ip, dport: dst_port}
-      p = {src: src, dst: dst, iface_name: iface, host: Socket.gethostname,
+    begin
+      if TCPPacket.can_parse?(pkt)
+        tcp_packet = TCPPacket.parse(pkt)
+        src_mac = EthHeader.str2mac(tcp_packet.eth_src).to_s
+        dst_mac = EthHeader.str2mac(tcp_packet.eth_dst).to_s
+        src_ip = IPHeader.octet_array(tcp_packet.ip_src).join('.')
+        dst_ip = IPHeader.octet_array(tcp_packet.ip_dst).join('.')
+        src_port = tcp_packet.tcp_src.to_s
+        dst_port = tcp_packet.tcp_dst.to_s
+        # p "tcp => (#{src_mac} - #{dst_mac}) => (#{src_ip} - #{dst_ip}) => (#{src_port} - #{dst_port})"
+        src = {smac: src_mac, sip: src_ip, sport: src_port}
+        dst = {dmac: dst_mac, dip: dst_ip, dport: dst_port}
+        p = {src: src, dst: dst, iface_name: iface, host: Socket.gethostname,
              content: pkt.force_encoding("ISO-8859-1").encode("UTF-8"),
              type: "tcp"}
-      # send_server(pkt)
-      pkts << p
-    elsif UDPPacket.can_parse?(pkt)
-      udp_packet = UDPPacket.parse(pkt)
-      src_mac = EthHeader.str2mac(udp_packet.eth_src).to_s
-      dst_mac = EthHeader.str2mac(udp_packet.eth_dst).to_s
-      src_ip = IPHeader.octet_array(udp_packet.ip_src).join('.')
-      dst_ip = IPHeader.octet_array(udp_packet.ip_dst).join('.')
-      src_port = udp_packet.udp_src.to_s
-      dst_port = udp_packet.udp_dst.to_s
-      # p "udp => (#{src_mac} - #{dst_mac}) => (#{src_ip} - #{dst_ip}) => (#{src_port} - #{dst_port})"
-      src = {smac: src_mac, sip: src_ip, sport: src_port}
-      dst = {dmac: dst_mac, dip: dst_ip, dport: dst_port}
-      p = {src: src, dst: dst, iface_name: iface, host: Socket.gethostname,
-             content: pkt.force_encoding("ISO-8859-1").encode("UTF-8"),
-             type: "udp"}
-      # send_server(pkt)
-      pkts << p
+        # send_server(pkt)
+        pkts << p
+      elsif UDPPacket.can_parse?(pkt)
+        udp_packet = UDPPacket.parse(pkt)
+        unless udp_packet.ip_src.nil?
+          src_mac = EthHeader.str2mac(udp_packet.eth_src).to_s
+          dst_mac = EthHeader.str2mac(udp_packet.eth_dst).to_s
+          src_ip = IPHeader.octet_array(udp_packet.ip_src).join('.')
+          dst_ip = IPHeader.octet_array(udp_packet.ip_dst).join('.')
+          src_port = udp_packet.udp_src.to_s
+          dst_port = udp_packet.udp_dst.to_s
+          # p "udp => (#{src_mac} - #{dst_mac}) => (#{src_ip} - #{dst_ip}) => (#{src_port} - #{dst_port})"
+          src = {smac: src_mac, sip: src_ip, sport: src_port}
+          dst = {dmac: dst_mac, dip: dst_ip, dport: dst_port}
+          p = {src: src, dst: dst, iface_name: iface, host: Socket.gethostname,
+               content: pkt.force_encoding("ISO-8859-1").encode("UTF-8"),
+               type: "udp"}
+          # send_server(pkt)
+          pkts << p
+        end
+      end
+      if pkts.size > 300
+        send_server(pkts)
+        pkts = []
+      end
     end
-    if pkts.size > 300
-      send_server(pkts)
-      pkts = []
-    end
+  rescue => e
+    p "failed"
+    p e
   end
 end
 
